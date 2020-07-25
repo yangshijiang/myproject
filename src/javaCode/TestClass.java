365876016ff3f086adf3d55740cfdc02acbaf9d9
wangshiqi <3013557330@qq.com>
2020-07-25 22:05:25 +0800

    添加类

--- src/javaCode/TestClass.java
+++ src/javaCode/TestClass.java
@@ -1,9 +1,14 @@
 package javaCode;
 
+
+
 /**
  * @Author: 王士奇
  * @Description: 1212
  * @Date: 2020/7/25 21:50
  */
 public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
